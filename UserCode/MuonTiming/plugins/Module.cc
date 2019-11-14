7283631ef088019cebcc9d06c690a896f86ab8a6
cericeci <cericeci@cern.ch>
2019-11-15 00:30:56 +0100

    Added an adaptation of the muon times module to run on top of MiniAOD

---
+++ UserCode/MuonTiming/plugins/Module.cc
@@ -0,0 +1,8 @@
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "UserCode/MuonTiming/plugins/MuonTimingProducer_Mini.h"
+
+DEFINE_FWK_MODULE(MuonTimingProducer_Mini);
