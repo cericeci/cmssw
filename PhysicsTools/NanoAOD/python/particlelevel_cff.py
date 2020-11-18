3d62e4cfb07314c17c3a7d92ec7a7ec943ca2720
cericeci <cericeci@cern.ch>
2020-11-18 13:57:11 +0100

    Increased range of dressedleptons to make them usable for polarization predictions

--- PhysicsTools/NanoAOD/python/particlelevel_cff.py
+++ PhysicsTools/NanoAOD/python/particlelevel_cff.py
@@ -31,11 +31,11 @@ particleLevel = cms.EDProducer("ParticleLevelProducer",
     excludeNeutrinosFromJetClustering = cms.bool(True),
     
     particleMinPt  = cms.double(0.),
-    particleMaxEta = cms.double(5.), # HF range. Maximum 6.0 on MiniAOD
+    particleMaxEta = cms.double(1.e99), # HF range. Maximum 6.0 on MiniAOD
     
     lepConeSize = cms.double(0.1), # for photon dressing
-    lepMinPt    = cms.double(15.),
-    lepMaxEta   = cms.double(2.5),
+    lepMinPt    = cms.double(0.),
+    lepMaxEta   = cms.double(1.e99),
     
     jetConeSize = cms.double(0.4),
     jetMinPt    = cms.double(10.),
